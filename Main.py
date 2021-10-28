f61aa8c449d66793ba6f9c4c4af88e7509c7ac11
mooncityorg <cryptomoon116@gmail.com>
2021-10-28 06:06:35 -0500

    dashboard introduced

---
+++ Main.py
@@ -0,0 +1,19 @@
+from dash import dcc
+from dash import Dash
+from dash import html
+
+
+application = Dash(suppress_callback_exceptions = True)
+server = application.server
+
+
+application.layout = html.Div([
+
+    html.H1('hello')
+
+])
+
+
+if (__name__ == '__main__'):
+
+    application.run_server(debug = True)
