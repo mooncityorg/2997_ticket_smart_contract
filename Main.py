88a8b44c2650a4efe7816cf7d42b05a3255707f1
mooncityorg <cryptomoon116@gmail.com>
2021-10-29 01:30:13 -0500

    directory adjustment, transition to login UI

--- Main.py
+++ Main.py
@@ -4,22 +4,16 @@ from dash import html
 
 from backEnd.Tool import Tool
 
+
 application = Dash(suppress_callback_exceptions = True)
 server = application.server
 
 
 application.layout = html.Div([
 
-    html.Div(id = 'layoutId',
-             children = [
 
-                html.H1('ok')
 
-             ]
-
-    )
-
-], style = {'background-image' : 'url(https://www.umkc.edu/global-assets/images/Womens-Soccer-at-the-Liberty-Memorial.jpg)'})
+])
 
 
 if (__name__ == '__main__'):
