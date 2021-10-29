c5bf4a649f64ad54806ff9e24d89e4a5b1b3fced
mooncityorg <cryptomoon116@gmail.com>
2021-10-29 00:04:41 -0500

    directories added

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ from dash import dcc
 from dash import Dash
 from dash import html
 
+from Back End.Tools import
 
 application = Dash(suppress_callback_exceptions = True)
 server = application.server
@@ -9,9 +10,14 @@ server = application.server
 
 application.layout = html.Div([
 
-    html.H1('3ASJW6')
+    html.Div(children = [
 
-])
+        html.H1(children = 'ok'),
+        html.H1(children = 'me')
+
+    ])
+
+], style = {'background-image' : 'url(https://www.umkc.edu/global-assets/images/Womens-Soccer-at-the-Liberty-Memorial.jpg)'})
 
 
 if (__name__ == '__main__'):
---
+++ backEnd/Tools.py
@@ -0,0 +1,9 @@
+from os import path
+
+
+class Tools:
+
+    def __init__(self):
+        '''  '''
+
+        self.path = path.realpath(__file__)
