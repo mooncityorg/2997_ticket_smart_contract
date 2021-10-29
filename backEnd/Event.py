abc3bd701e73d1be842118ac0384a1f749be1c80
mooncityorg <cryptomoon116@gmail.com>
2021-10-29 01:53:43 -0500

    person methods

--- backEnd/Event.py
+++ backEnd/Event.py
@@ -1,6 +1,11 @@
 
 class Event:
 
-    def __init__(self):
-        self.type, self.location, self.topic, self.classNum = str()
-        self.time = int()
+    # input should be list in specified order
+    def __init__(self, info):
+        self.ID, self.type, self.time, self.location, self.organizer, self.invitee, self.classNum, self.topic, self.notes = info
+
+    # updates database from user input
+    # def update(self, info):
+
+
--- backEnd/Person.py
+++ backEnd/Person.py
@@ -1,7 +1,32 @@
+import Event
 
 class Person:
 
-    def __init__(self):
-        self.role, self.name, self.email, self.phProv, self.phNum, self.password, self.department = str()
-        self.createAppt, self.cancelAppt, self.editAppt, self.viewStud = bool()
-        self.classList, self.apptList = list()
+    def __init__(self, info):
+        self.role, self.name, self.email, self.phNum, self.phProv, self.password, self.department, self.createBool, self.cancelBool, self.editBool, self.viewStud, self.classList, self.apptList = info
+
+    def createAppt(self, info):
+        if self.createBool:
+            event = Event(info)
+            self.apptList.append(event)
+
+    def cancelAppt(self, ID):
+        if self.cancelBool:
+            # how do you use list methods when referencing class attributes?
+            # self.apptList.index(Event.getattr(ID), ID)
+            for j in self.apptList:
+                if j.ID == ID: self.apptList.remove(j)
+
+    # def editAppt(self, info):
+    #     if self.editBool:
+    #         # pass and mostly empty list? # need to parse info?
+    #         for j in self.apptList:
+    #             if j.ID == info[0]:
+                    # assign changed values
+
+
+    # def checkAppt(self, time):
+        # check if there is an upcoming event?
+
+    # updates database from user input
+    # def update(self, info):
