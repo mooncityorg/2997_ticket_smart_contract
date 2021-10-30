a2e42839c1f4ab7b65c09acda5f1d3dc4e3972fb
mooncityorg <cryptomoon116@gmail.com>
2021-10-30 04:54:01 -0500

    UI updates 10/30

--- frontEnd/Layout/Login.py
+++ frontEnd/Layout/Login.py
@@ -4,36 +4,177 @@ from backEnd.Tool import getJSON
 
 
 style = getJSON(file = '/frontEnd/Resource/Login.json')
-loginLayout = html.Div(id = 'divId',
-                       children = [
 
-                           # Background <
-                           html.Div(id = 'divDivId',
-                                    children = [
+#---crud code -========================
+# loginLayout = html.Div(id = 'divId',
+#                        children = [
+#
+#                            # Background <
+#                            html.Div(id = 'divDivId',
+#                                     children = [
+#
+#                                         # UMKC Logo <
+#                                         html.Div(id = 'divDivDivIdA',
+#                                                  children = [
+#
+#                                                      html.Img(src = style['divDivDivImgSrc'],
+#                                                               style = style['divDivDivImgStyle'])
+#
+#                                                  ], style = style['divDivDivStyleA']),
+#
+#                                         # email Input <
+#                                         html.Div(id = 'EmailEntry',
+#                                                  children = [
+#                                                     dcc.Input(
+#                                                          id = 'email',
+#                                                          type = "email",
+#                                                          placeholder  = "Enter email",
+#                                                          debounce=True,
+#                                                          autoComplete = "on",
+#
+#                                                      ),
+#                                                  ], style = style['divDivDivStyleB']),
+#
+#                                         # password Input <
+#                                         html.Div(id='PasswordEntry',
+#                                                  children = [
+#                                                     dcc.Input(
+#                                                          id = 'Password',
+#                                                          type = "password",
+#                                                          placeholder  = "Enter password",
+#                                                          debounce=True,
+#                                                          autoComplete = "on",
+#
+#                                                      ),
+#
+#                                                  ])
+#                            ],
+#                                     style = style['divDivStyle']),
+#
+#                                         html.Div(id='SubmitButton',
+#                                             children = [
+#                                                 html.Button('Submit', id='submit-val', n_clicks=0
+#                                         )
+#                                     ],
+#                                         style = style['Button'])
+#
+#                       ], style = style['divStyle'])
+#
+# # this is where callback goes
+# # we grab states of username and password
+# # input is button id # output is uncertain
+# # back end is called here
 
-                                        # UMKC Logo <
-                                        html.Div(id = 'divDivDivIdA',
-                                                 children = [
+#not crud code but styling integrated =-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-                                                     html.Img(src = style['divDivDivImgSrc'],
-                                                              style = style['divDivDivImgStyle'])
+loginLayout = html.Div(
+    children = [
+        html.Div(style = {
+            "top": 0,
+            "left" : 0,
+            "right": 0,
+            "bottom": 0,
+            "textAlign": "center",
+            "position": "fixed",
+            "backgroundColor" : "#04487F"}),
+            html.Div(
+                children=[
+                    html.Div(style={"top": 60,
+                        "left": 80,
+                        "right": 80,
+                        "bottom": 100,
+                        "borderRadius": 10,
+                        "padding": 10,
+                        "textAlign": "center",
+                        "position": "absolute",
+                        "border": "5px solid #FFD52F",
+                        "backgroundPosition": "center",
+                        "background": "url(https://www.umkc.edu/global-assets/images/Womens-Soccer-at-the-Liberty-Memorial.jpg)"}),
+                        html.Div(
+                            children=[
+                                html.Div(html.Img(src=style['divDivDivImgSrc']),
+                                        style={"top": 60,
+                                        "left": 80,
+                                        "right": 80,
+                                        "bottom": 100,
+                                        "borderRadius": 10,
+                                        "padding": 10,
+                                        "textAlign": "center",
+                                        "position": "absolute",
+                                        "border": "5px solid #FFD52F",
+                                        "backgroundPosition": "center"}),
+                                html.Div(
+                                    children=[
+                                        html.Div(style = {
+                                            "width": 380,
+                                            "height": 315,
+                                            "border": 1,
+                                            "backgroundColor": "#FFD52F ",
+                                            "position": "absolute",
+                                            "left": 767,
+                                            "marginTop": 70,
+                                            "bottom" : 382,
+                                            "border-radius" : 25,
+                                            }),
+                                    html.Div(
+                                        children=[
+                                            html.Div(style = {
+                                                "width": 365,
+                                                "height": 300,
+                                                "border": 1,
+                                                "backgroundColor": "#04487F",
+                                                "position": "absolute",
+                                                "left": 775,
+                                                "marginTop": 70,
+                                                "bottom" : 390,
+                                                "border-radius" : 25,
+                                                }),
+                                            html.Div (
+                                                children = [
+                                                    html.Div(dcc.Input(
+                                                        id = 'email',
+                                                        type = "email",
+                                                        placeholder  = "Enter email",
+                                                        debounce=True,
+                                                        autoComplete = "on"), style = {
+                                                        "bottom": 575,
+                                                        "left": 870,
+                                                        "position": "absolute",
+                                                        "border-radius" : 25}),
+                                                    html.Div(
+                                                        children=[
+                                                            html.Div(dcc.Input(
+                                                              id = 'Password',
+                                                              type = "password",
+                                                              placeholder  = "Enter password",
+                                                              debounce=True,
+                                                              autoComplete = "on",), style ={
+                                                              "bottom": 545,
+                                                              "left": 870,
+                                                              "position": "absolute",
+                                                              "border-radius" : 25}),
+                                                            html.Button(id = "Button",
+                                                                children=[html.Img(src = "submitbutton")]),
+                                                                    html.Div(html.Button(
+                                                                        'Submit',
+                                                                        id='submit-val',
+                                                                        n_clicks=0,
+                                                                        style = {
+                                                                        "bottom": 515,
+                                                                        "left": 930,
+                                                                        "position": "absolute",
+                                                                        }))
+                                                            ]
+                                                        )
+                                                    ]
+                                                )
+                                            ]
+                                        )
+                                    ]
+                                )
 
-                                                 ], style = style['divDivDivStyleA']),
+                            ]
+                        )
+                    ]
+                )])
 
-                                        # >
-
-                                        # Input <
-                                        html.Div(id = 'divDivDivIdB',
-                                                 children = [
-
-
-
-                                                 ], style = style['divDivDivStyleB'])
-
-                                        # >
-
-                                    ], style = style['divDivStyle'])
-
-                           # >
-
-                       ], style = style['divStyle'])
