c1c07097f878c422a17b1107e8cbd32879d5a41e
mooncityorg <cryptomoon116@gmail.com>
2021-10-30 00:11:52 -0500

    background resizing patch

--- frontEnd/Layout/Login.py
+++ frontEnd/Layout/Login.py
@@ -4,36 +4,44 @@ from backEnd.Tool import getJSON
 
 
 style = getJSON(file = '/frontEnd/Resource/Login.json')
-loginLayout = html.Div(id = 'divId',
+loginLayout = html.Div(id = 'layoutId',
                        children = [
 
                            # Background <
-                           html.Div(id = 'divDivId',
+                           html.Div(id = 'divBackgroundId',
                                     children = [
 
-                                        # UMKC Logo <
-                                        html.Div(id = 'divDivDivIdA',
+                                        # Image <
+                                        html.Div(id = 'divImageId',
                                                  children = [
 
-                                                     html.Img(src = style['divDivDivImgSrc'],
-                                                              style = style['divDivDivImgStyle'])
+                                                     html.Img(src = style['imgSrc'],
+                                                              style = style['imgStyle'])
 
-                                                 ], style = style['divDivDivStyleA']),
+                                                 ], style = style['divImageStyle']),
 
                                         # >
 
                                         # Input <
-                                        html.Div(id = 'divDivDivIdB',
+                                        html.Div(id = 'divInputId',
                                                  children = [
 
 
 
-                                                 ], style = style['divDivDivStyleB'])
+                                                 ], style = style['divInputStyle'])
 
                                         # >
 
-                                    ], style = style['divDivStyle'])
+                                    ], style = style['divBackgroundStyle']),
+
+                           # Markdown <
+                           html.Div(id = 'divMarkdownId',
+                                    children = [
+
+
+
+                                    ])
 
                            # >
 
-                       ], style = style['divStyle'])
+                       ], style = style['layoutStyle'])
