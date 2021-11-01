cfc8550f0538e96dcf20789cc67bc8cf0a1dba62
mooncityorg <cryptomoon116@gmail.com>
2021-11-01 03:27:47 -0500

    position check

--- Main.py
+++ Main.py
@@ -1,16 +1,11 @@
-# from dash import Dash
-# from frontEnd.Layout.Login import mainLayout
-# import dash
-#
-# # application = Dash(suppress_callback_exceptions = True)
-# # server, application.layout = application.server, mainLayout()
-#
-# app = dash.Dash(__name__)
-# app.layout = mainLayout()
-#
-# if (__name__ == '__main__'):
-#     app.run_server(debug = True)
-import dash
-app = dash.Dash(__name__)
+from dash import Dash
+from frontEnd.Layout.Login import loginLayout
 
 
+application = Dash(suppress_callback_exceptions = True)
+server, application.layout = application.server, loginLayout
+
+
+if (__name__ == '__main__'):
+
+    application.run_server(debug = True)
