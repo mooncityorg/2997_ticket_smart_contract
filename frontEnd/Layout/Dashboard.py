1c1019b9ea66682303a6799cc6355d52e430855e
mooncityorg <cryptomoon116@gmail.com>
2021-11-12 18:25:33 -0600

    building commands for queries

--- frontEnd/Layout/Dashboard.py
+++ frontEnd/Layout/Dashboard.py
@@ -3,6 +3,9 @@ from dash import html, dcc
 
 # >
 
+# note: we get a user's data in here
+# and not in Login.py. in Login.py we
+# set the data for a user.
 
 # Declaration <
 dashboardLayout = html.Div(id = 'dashboardLayoutId',
