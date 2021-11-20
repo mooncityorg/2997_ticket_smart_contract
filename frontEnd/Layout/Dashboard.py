3cb9489cf44a6da98690127a574afa60c8d78f0f
mooncityorg <cryptomoon116@gmail.com>
2021-11-20 04:01:55 -0600

    Home is introduced. An adjustment of loading data is made to Home.py

--- frontEnd/Layout/Dashboard.py
+++ frontEnd/Layout/Dashboard.py
@@ -3,16 +3,16 @@ from dash import html, dcc
 
 # >
 
-# note: we get a user's data in here
-# and not in Login.py. in Login.py we
-# set the data for a user.
 
 # Declaration <
+
+
+# >
+
+
 dashboardLayout = html.Div(id = 'dashboardLayoutId',
                            children = [
 
                                html.H1('ok')
 
                            ])
-
-# >
---
+++ frontEnd/Layout/Home.py
@@ -0,0 +1,12 @@
+# Import <
+
+
+# >
+
+
+# Declaration <
+# note: we get a user's data in here
+# and not in Login.py. in Login.py we
+# set the data for a user.
+
+# >
