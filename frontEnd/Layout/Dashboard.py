fdd44c52c9b29e8790cf383488f8de873df9d9ec
mooncityorg <cryptomoon116@gmail.com>
2021-12-10 11:38:27 -0600

    dashboard grid establishment

--- frontEnd/Layout/Dashboard.py
+++ frontEnd/Layout/Dashboard.py
@@ -1,5 +1,13 @@
 # Import <
 from dash import html, dcc
+import dash_bootstrap_components as dbc
+
+
+# >
+
+
+# Declaration <
+
 
 # >
 
@@ -7,4 +15,29 @@ from dash import html, dcc
 def dashboardLayout(userId):
     '''  '''
 
-    return html.H1('dashboard')
+    return dbc.Row(children = [
+
+        dbc.Col(children = [
+
+            dbc.Row(html.H1('1'), style = {'backgroundColor' : '#0066cc'}),
+            dbc.Row(html.H1('2'), style = {'backgroundColor' : '#0066cc'}),
+            dbc.Row(html.H1('3'), style = {'backgroundColor' : '#0066cc'})
+
+        ]),
+
+        dbc.Col(children = [
+
+            dbc.Row(html.H1('4'), style = {'backgroundColor' : '#0066cc',
+                                            'height' : '40vh'})
+
+        ]),
+
+        dbc.Col(children = [
+
+            dbc.Row(html.H1('5'), style = {'backgroundColor' : '#0066cc',
+                                            'height' : '30vh'}),
+            dbc.Row(html.H1('6'), style = {'backgroundColor' : '#0066cc'})
+
+        ])
+
+    ])
